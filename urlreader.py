36c7616e58a3b236a48200edbb0362d430e48cf3
nacho22 <ignacio.rustan@gmail.com>
2019-12-26 18:19:06 -0300

    using external file with multiple lines and write to csv

--- urlreader.py
+++ urlreader.py
@@ -1,17 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
+
+with open('output.csv', 'w', newline='') as output:
+	writer = csv.writer(output)
+	writer.writerow(["Link", "Price", "Address", "Surface", "Location"])
 
 with open('apts.txt', 'r') as reader:
 	for line in reader:
 		url = line.rstrip('\n')
 		response = requests.get(url)
-		print(response.request.url)
+		print("Working...")
+		#print(response.request.url)
 		soup = BeautifulSoup(response.content, "html.parser")
 		myprice = soup.find("span", {"class": "price-tag-fraction"})
-		print(myprice)
+		#print(myprice.text)
 		myaddress = soup.find("h2", {"class": "map-address"})
-		print(myaddress)
+		#print(myaddress.text)
 		mysurface = soup.find("dd", {"class": "align-surface"})
-		print(mysurface)
+		#print(mysurface.text)
 		mylocation = soup.find("h3", {"class": "map-location"})
-		print(mylocation)
+		#print(mylocation.text)
+
+		with open('output.csv', 'a', newline='') as output:
+			writer = csv.writer(output)
+			writer.writerow([url, myprice.text, myaddress.text, mysurface.text, mylocation.text])
