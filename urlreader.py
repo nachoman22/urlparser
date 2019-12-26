63b1b11e59f5d925b52861acbb3d3be8a42c6a18
nacho22 <ignacio.rustan@gmail.com>
2019-12-26 00:34:11 -0300

    trying to use a feed file

--- urlreader.py
+++ urlreader.py
@@ -1,19 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://departamento.mercadolibre.com.ar/MLA-831154167-de-revista-espect-amplio-dpto-c-coch-y-renta-_JM"
-response = requests.get(url)
-soup = BeautifulSoup(response.content, "html.parser")
-
-myprice = soup.find("span", {"class": "price-tag-fraction"})
-print(myprice)
-myprice.decompose()
-myaddress = soup.find("h2", {"class": "map-address"})
-print(myaddress)
-myaddress.decompose()
-mysurface = soup.find("dd", {"class": "align-surface"})
-print(mysurface)
-mysurface.decompose()
-mylocation = soup.find("h3", {"class": "map-location"})
-print(mylocation)
-mylocation.decompose()
+with open('apts.txt', 'r') as reader:
+	for line in reader:
+		print(line, end='')
+		response = requests.get(line)
+		soup = BeautifulSoup(response.content, "html.parser")
+		myprice = soup.find("span", {"class": "price-tag-fraction"})
+		print(myprice)
+		myaddress = soup.find("h2", {"class": "map-address"})
+		print(myaddress)
+		mysurface = soup.find("dd", {"class": "align-surface"})
+		print(mysurface)
+		mylocation = soup.find("h3", {"class": "map-location"})
+		print(mylocation)
