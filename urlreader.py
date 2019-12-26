e699cdf45564e366961ae627796b1e727f71588b
nacho22 <ignacio.rustan@gmail.com>
2019-12-26 19:53:31 -0300

    write to files improved and error out separated

--- urlreader.py
+++ urlreader.py
@@ -6,22 +6,27 @@ with open('output.csv', 'w', newline='') as output:
 	writer = csv.writer(output)
 	writer.writerow(["Link", "Price", "Address", "Surface", "Location"])
 
+with open('output.err', 'w', newline='') as errout:
+	writer = csv.writer(errout)
+	writer.writerow(["Missing values for the following links"])
+
 with open('apts.txt', 'r') as reader:
 	for line in reader:
 		url = line.rstrip('\n')
 		response = requests.get(url)
-		print("Working...")
-		#print(response.request.url)
 		soup = BeautifulSoup(response.content, "html.parser")
 		myprice = soup.find("span", {"class": "price-tag-fraction"})
-		#print(myprice.text)
 		myaddress = soup.find("h2", {"class": "map-address"})
-		#print(myaddress.text)
 		mysurface = soup.find("dd", {"class": "align-surface"})
-		#print(mysurface.text)
 		mylocation = soup.find("h3", {"class": "map-location"})
-		#print(mylocation.text)
 
-		with open('output.csv', 'a', newline='') as output:
-			writer = csv.writer(output)
-			writer.writerow([url, myprice.text, myaddress.text, mysurface.text, mylocation.text])
+		if myprice != None and myaddress != None and mysurface != None and mylocation != None:
+			print("Data recorded for", url)
+			with open('output.csv', 'a', newline='') as output:
+				writer = csv.writer(output)
+				writer.writerow([url, myprice.text, myaddress.text, mysurface.text, mylocation.text])
+		else:
+			print("Missing value on the output for", url)
+			with open('output.err', 'a', newline='') as errout:
+				writer = csv.writer(errout)
+				writer.writerow([url])
