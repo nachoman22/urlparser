c8567396f4ff72db0e29b99872b8ed59d1d7b4b5
diego-bandin <diego.bandin@workday.com>
2019-12-27 23:16:45 +0000

    Change in the workflow to reduce the nesting and complexity.

--- urlreader.py
+++ urlreader.py
@@ -3,26 +3,37 @@ from bs4 import BeautifulSoup
 import csv
 import logging
 
-logging.basicConfig(filename='urlreader.log',level=logging.INFO)
+logging.basicConfig(filename='urlreader.log', level=logging.INFO)
+
+# Open file and read urls into dictionary. File descriptor is closed
+# when out of the "with"
+with open('apts.txt', 'r') as reader:
+    url_list = reader.readlines()
+
+# Initialize list for csv rows
+csv_list = []
+# Main logic
+for url_item in url_list:
+    url = url_item.rstrip('\n')
+    # Retrieve response from url and separate desired items
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    myprice = soup.find("span", {"class": "price-tag-fraction"})
+    myaddress = soup.find("h2", {"class": "map-address"})
+    mysurface = soup.find("dd", {"class": "align-surface"})
+    mylocation = soup.find("h3", {"class": "map-location"})
+
+    if myprice != None and myaddress != None and mysurface != None and mylocation != None:
+        logging.info('Data recorded for: ' + url)
+        csv_list.append([url, myprice.text, myaddress.text, mysurface.text, mylocation.text])
+    else:
+        logging.warning('Missing value on the report for: ' + url)
 
 with open('report.csv', 'w', newline='') as report:
     writer = csv.writer(report)
+    # Write header
     writer.writerow(["Link", "Price", "Address", "Surface", "Location"])
-
-with open('apts.txt', 'r') as reader:
-    for line in reader:
-        url = line.rstrip('\n')
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, "html.parser")
-        myprice = soup.find("span", {"class": "price-tag-fraction"})
-        myaddress = soup.find("h2", {"class": "map-address"})
-        mysurface = soup.find("dd", {"class": "align-surface"})
-        mylocation = soup.find("h3", {"class": "map-location"})
-
-        if myprice != None and myaddress != None and mysurface != None and mylocation != None:
-            logging.info('Data recorded for: ' + url)
-            with open('report.csv', 'a', newline='') as report:
-                writer = csv.writer(report)
-                writer.writerow([url, myprice.text, myaddress.text, mysurface.text, mylocation.text])
-        else:
-            logging.warning('Missing value on the report for: ' + url)
+    # Write rows from list
+    for row in csv_list:
+        writer = csv.writer(report)
+        writer.writerow(row)
